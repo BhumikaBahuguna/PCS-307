d466ea92e8412c33d4ca98135e921c46084ea36a
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2024-11-13 20:22:32 +0530

    Add files via upload

---
+++ static_var_wo_class.cpp
@@ -0,0 +1,75 @@
+//STATIC VARIABLE WITHOUT CLASS
+/*
+#include<iostream>
+using namespace std;
+void show(){
+static int x;
+cout<<x;
+}
+int main(){
+    show();
+    return 0;
+}
+*/
+
+
+/*
+#include<iostream>
+using namespace std;
+void show(){
+static int x=10;
+cout<<x;
+}
+int main(){
+    show();
+    return 0;
+}
+*/
+
+
+/*
+#include<iostream>
+using namespace std;
+void show(){
+static int x;
+x++;
+cout<<x<<endl;
+}
+int main(){
+    show();
+    show();
+    return 0;
+}
+*/
+
+
+/*
+#include<iostream>
+using namespace std;
+void show(){
+int x;
+x++;
+cout<<x<<endl;
+}
+int main(){
+    show();
+    show();
+    return 0;
+}
+*/
+
+
+/*
+#include<iostream>
+using namespace std;
+void show(){
+int x=11;
+x++;
+cout<<x<<endl;
+}
+int main(){
+    show();
+    show();
+    return 0;
+}
+*/
