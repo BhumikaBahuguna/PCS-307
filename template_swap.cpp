6138391a70fff9481b1177c37cbd04e9788827e7
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2024-11-22 10:30:19 +0530

    template_swap.cpp

--- template_swap.cpp
+++ template_swap.cpp
@@ -19,3 +19,26 @@ cout<<a<<" "<<b<<endl;
 swap(&x,&y);
 cout<<x<<" "<<y<<endl;
 }
+
+
+
+/*#include<iostream>
+using namespace std;
+template <typename T>
+ T swap(T *a , T *b){     //WE HAVE TO RETURN SOMETHING
+T c;
+c=*a;
+*a=*b;
+*b=c;
+}
+int main(){
+int a,b;
+cin>>a>>b;
+char x,y;
+cin>>x>>y;
+swap(&a,&b);
+cout<<a<<" "<<b<<endl;
+swap(&x,&y);
+cout<<x<<" "<<y<<endl;
+}
+*/
