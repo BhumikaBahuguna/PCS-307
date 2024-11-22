f41ecff04ad36d812136ebaa0e54d0629bdeccba
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2024-11-22 11:11:49 +0530

    exception3.cpp

--- exception3.cpp
+++ exception3.cpp
@@ -23,3 +23,37 @@ cout<<"exception caught";                  //IF APROPRIATE CATCH NOT PRESENT TO 
 }                                          //ELLIPSISS CATCHES ANY TYPE PF ERROR
 return 0;
 }
+
+
+
+
+
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+int main(){
+int a=20;
+try{
+if(a==10)
+throw 10;
+if(a==30)
+throw "hello";
+if(a==20)
+throw 100;
+}
+catch(...){                           gives error
+cout<<"exception caught";
+}
+catch(string s){
+cout<<"string exception caught";
+}
+catch(double){
+cout<<"integer exception caught";
+}
+return 0;
+}
+*/
