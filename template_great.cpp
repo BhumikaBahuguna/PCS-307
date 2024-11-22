99952b6c135220f1baf2278f29a26b3fcafd820b
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2024-11-22 10:34:57 +0530

    template_great.cpp

---
+++ template_great.cpp
@@ -0,0 +1,14 @@
+FIND GREATER OF TWO NUMBER/CHARAACTER USING TEMPLATE FUNCTION
+
+
+#include<iostream>
+using namespace std;
+template <typename T>
+ T great(T a , T b){
+return(a>b? a:b);
+}
+int main(){
+cout<<great(10,30)<<endl;
+cout<<great(20.5,10.5)<<endl;
+cout<<great('a','n')<<endl;
+}
