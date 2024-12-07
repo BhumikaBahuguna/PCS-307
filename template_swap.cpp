7d1b730b8fd4d10f861bbf8dcc8285438791c6e2
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2024-12-07 14:29:53 +0530

    Add files via upload

--- template_swap.cpp
+++ template_swap.cpp
@@ -1,5 +1,3 @@
-USE TEMPLATE FUNCTION TO SWAP TO INTEGERS AND TWO CHARACTERS
-
 #include<iostream>
 using namespace std;
 template <typename T>
@@ -19,26 +17,3 @@ cout<<a<<" "<<b<<endl;
 swap(&x,&y);
 cout<<x<<" "<<y<<endl;
 }
-
-
-
-/*#include<iostream>
-using namespace std;
-template <typename T>
- T swap(T *a , T *b){     //WE HAVE TO RETURN SOMETHING
-T c;
-c=*a;
-*a=*b;
-*b=c;
-}
-int main(){
-int a,b;
-cin>>a>>b;
-char x,y;
-cin>>x>>y;
-swap(&a,&b);
-cout<<a<<" "<<b<<endl;
-swap(&x,&y);
-cout<<x<<" "<<y<<endl;
-}
-*/
